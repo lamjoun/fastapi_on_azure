f3dd914e46a6049bd88cfa0ec35b60a2d120cc29
Rachid LAMJOUN <93473696+lamjoun@users.noreply.github.com>
2024-04-25 12:11:04 +0200

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,2 +1,2 @@
 # az webapp config set --resource-group rgtest --name rlapp13 --startup-file app.py
-uvicorn --bind=0.0.0.0 --timeout 600 --port 8080 app.main:app
+uvicorn --bind=0.0.0.0 --timeout 600 --port 8080 app:app
